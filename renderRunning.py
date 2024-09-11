a79086e8f54a8a3b9a0d468e9f68e7abd311acc8
Fardin <faa35@sfu.ca>
2024-09-10 21:30:52 -0700

    sending periodic requests, keeping server active#3

--- renderRunning.py
+++ renderRunning.py
@@ -5,15 +5,17 @@ url = 'https://journey-genie-v2.onrender.com/'
 
 def keep_website_active():
     try:
+        count = 1  # Start counting from 1
         while True:
             response = requests.get(url)
             if response.status_code == 200:
-                print(f"Website is up and running! Status code: {response.status_code}")
+                print(f"{count}. Website is up and running! Status code: {response.status_code}")
             else:
-                print(f"Failed to connect. Status code: {response.status_code}")
+                print(f"{count}. Failed to connect. Status code: {response.status_code}")
             
-            # Wait for 40 seconds before sending another request
-            time.sleep(40)  # meaning 40 seconds
+            # Increment the count and wait for 40 seconds before sending another request
+            count += 1
+            time.sleep(40)  # 40 seconds
     except KeyboardInterrupt:
         print("Process stopped by the user.")
 
