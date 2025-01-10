6d7e31a9aac887943f421717bae7459b2a1fc782
Fardin <faa35@sfu.ca>
2025-01-09 21:05:03 -0800

    Add CORS configuration and APIs for my personal website

--- src/main/java/com/genie/journey_genie/JourneyGenieApplication.java
+++ src/main/java/com/genie/journey_genie/JourneyGenieApplication.java
@@ -3,6 +3,10 @@ package com.genie.journey_genie;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
 
 @SpringBootApplication
 public class JourneyGenieApplication {
@@ -10,5 +14,17 @@ public class JourneyGenieApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(JourneyGenieApplication.class, args);
 	}
+	@Bean
+    public WebMvcConfigurer corsFixer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**") 
+				.allowedOrigins("http://localhost:3000/")
+				.allowedHeaders("*")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS");
+			}
+        };
+    }
 
 }
